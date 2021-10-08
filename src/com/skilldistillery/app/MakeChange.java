2447a48b6728f17a933756c3d1a3ac434d99e25e
wangchengni <wangchengni001@gmail.com>
2021-10-08 16:48:43 -0600

    main

--- src/com/skilldistillery/app/MakeChange.java
+++ src/com/skilldistillery/app/MakeChange.java
@@ -1,10 +1,11 @@
 package com.skilldistillery.app;
-
+import java.util.Scanner;
 public class MakeChange {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner kb = new Scanner(System.in);
 	}
 
 }
